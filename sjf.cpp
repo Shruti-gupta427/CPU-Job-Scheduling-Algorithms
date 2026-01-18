619337beb0681f6cb66a0dbba71ec9d1b50917a3
shrutiXcode <sg920450@gmail.com>
2026-01-18 15:54:39 +0530

    Implemented shortest job first

---
+++ sjf.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of processes: ";
+    cin >> n;
+
+    vector<int> pid(n), AT(n), BT(n), CT(n), TAT(n), WT(n);
+    vector<bool> done(n, false);
+
+    for (int i = 0; i < n; i++) {
+        pid[i] = i + 1;
+        cout << "Enter Arrival Time and Burst Time for P" << pid[i] << ": ";
+        cin >> AT[i] >> BT[i];
+    }
+
+    int completed = 0;
+    int currentTime = 0;
+
+    while (completed < n) {
+        int idx = -1;
+        int minBT = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            if (AT[i] <= currentTime && !done[i] && BT[i] < minBT) {
+                minBT = BT[i];
+                idx = i;
+            }
+        }
+        if (idx == -1) {
+            currentTime++;
+            continue;
+        }
+        CT[idx]=currentTime + BT[idx];
+        TAT[idx]=CT[idx] - AT[idx];
+        WT[idx]=TAT[idx] - BT[idx];
+
+        currentTime = CT[idx];
+        done[idx] = true;
+        completed++;
+    }
+
+    cout << "\nPID\tAT\tBT\tCT\tTAT\tWT\n";
+    for (int i = 0; i < n; i++) {
+        cout << pid[i] << "\t"
+             << AT[i] << "\t"
+             << BT[i] << "\t"
+             << CT[i] << "\t"
+             << TAT[i] << "\t"
+             << WT[i] << "\n";
+    }
+
+    return 0;
+}
