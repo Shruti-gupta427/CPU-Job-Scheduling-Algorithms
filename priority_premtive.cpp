cdfd743caf368035a724ae3e732b49c29bf947cf
shrutiXcode <sg920450@gmail.com>
2026-02-04 22:42:35 +0530

    few changes

--- priority_premtive.cpp
+++ priority_premtive.cpp
@@ -8,35 +8,66 @@ int main()
     cin >> n;
 
     vector<int> pid(n), AT(n), BT(n), CT(n), TAT(n), WT(n), PR(n), RT(n);
-        float avgTAT = 0, avgWT = 0;
+    float avgTAT = 0, avgWT = 0;
+
     for (int i = 0; i < n; i++)
     {
         pid[i] = i + 1;
-        cout << "Enter Arrival Time Burst Time and Priority P" << pid[i] << ": ";
+        cout << "Enter AT BT PR for P" << pid[i] << ": ";
         cin >> AT[i] >> BT[i] >> PR[i];
         RT[i] = BT[i];
     }
-    int completed = 0;
-    int currenttime = 0;
+
+    int completed = 0, currenttime = 0;
     vector<bool> done(n, false);
+
+    vector<tuple<int, int, int>> gantt;
+
+    int prev = -2;   
+    int startTime = 0;
+
     while (completed < n)
     {
         int idx = -1;
-        int maxPR = INT_MIN;
+        int minPR = INT_MAX;
+
         for (int i = 0; i < n; i++)
         {
-            if (AT[i] <= currenttime && PR[i] > maxPR && !done[i])
+            if (AT[i] <= currenttime && !done[i])
             {
-                maxPR = PR[i];
-                idx = i;
+                if (PR[i] < minPR) 
+                {
+                    minPR = PR[i];
+                    idx = i;
+                }
             }
         }
 
+       
         if (idx == -1)
         {
+            if (prev != -1)
+            {
+                if (prev != -2)
+                    gantt.push_back({prev, startTime, currenttime});
+
+                startTime = currenttime;
+                prev = -1;
+            }
             currenttime++;
             continue;
         }
+
+        
+        if (prev != idx)
+        {
+            if (prev != -2)
+                gantt.push_back({prev, startTime, currenttime});
+
+            startTime = currenttime;
+            prev = idx;
+        }
+
         RT[idx]--;
         currenttime++;
 
@@ -45,26 +76,49 @@ int main()
             CT[idx] = currenttime;
             TAT[idx] = CT[idx] - AT[idx];
             WT[idx] = TAT[idx] - BT[idx];
+
             avgWT += WT[idx];
             avgTAT += TAT[idx];
 
             done[idx] = true;
-
             completed++;
         }
     }
+
+ 
+    gantt.push_back({prev, startTime, currenttime});
+
+   
     cout << "\nPID\tAT\tBT\tPR\tCT\tTAT\tWT\n";
     for (int i = 0; i < n; i++)
     {
-        cout << pid[i] << "\t"
-             << AT[i] << "\t"
-             << BT[i] << "\t"
-             << PR[i] << "\t"
-             << CT[i] << "\t"
-             << TAT[i] << "\t"
-             << WT[i] << "\n";
+        cout << pid[i] << "\t" << AT[i] << "\t" << BT[i] << "\t"
+             << PR[i] << "\t" << CT[i] << "\t"
+             << TAT[i] << "\t" << WT[i] << "\n";
     }
-     cout << "Average Waiting Time = " << avgWT / n;
-    cout << " Average Turnaround Time = " << avgTAT / n << endl;
+
+    cout << "\nGantt Chart:\n";
+    for (auto &g : gantt)
+    {
+        int p, s, e;
+        tie(p, s, e) = g;
+        if (p == -1)
+            cout << "| IDLE ";
+        else
+            cout << "| P" << pid[p] << " ";
+    }
+    cout << "|\n";
+
+    for (auto &g : gantt)
+    {
+        int p, s, e;
+        tie(p, s, e) = g;
+        cout << s << "     ";
+    }
+    cout << currenttime << "\n";
+
+    cout << "\nAverage Waiting Time = " << avgWT / n;
+    cout << "\nAverage Turnaround Time = " << avgTAT / n << endl;
+
     return 0;
 }
