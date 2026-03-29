bd3055672acf7e104e866f20692166e0b8e49841
shrutiXcode <sg920450@gmail.com>
2026-03-29 11:28:20 +0530

    implemented bankers algo with few more features

---
+++ Advanced_banker.cpp
@@ -0,0 +1,183 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isSafe(int n, int m,
+vector<vector<int>> &allocation,
+vector<vector<int>> &need,
+vector<int> &available,
+vector<int> &safeSequence,
+vector<bool> &finished) {   
+
+    vector<int> work = available;
+    vector<bool> finish = finished;   
+
+    safeSequence.clear();
+    bool changed = true;
+
+    while (changed) {
+        changed = false;
+
+        for (int i = 0; i < n; i++) {
+            if (!finish[i]) {
+                bool canRun = true;
+
+                for (int j = 0; j < m; j++) {
+                    if (need[i][j] > work[j]) {
+                        canRun = false;
+                        break;
+                    }
+                }
+
+                if (canRun) {
+                    for (int j = 0; j < m; j++)
+                        work[j] += allocation[i][j];
+
+                    finish[i] = true;
+                    safeSequence.push_back(i);
+                    changed = true;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+        if (!finish[i])
+            return false;
+
+    return true;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> allocation(n, vector<int>(m));
+    vector<vector<int>> maxm(n, vector<int>(m));
+    vector<vector<int>> need(n, vector<int>(m));
+    vector<int> available(m);
+    vector<bool> finished(n, false);  
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> allocation[i][j];
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> maxm[i][j];
+
+    for (int j = 0; j < m; j++)
+        cin >> available[j];
+
+    cout << "Need Matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            need[i][j] = maxm[i][j] - allocation[i][j];
+            cout << need[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    vector<int> safeSequence;
+
+    if (isSafe(n, m, allocation, need, available, safeSequence, finished)) {
+        cout << "SAFE\nSafe Sequence: ";
+        for (int i : safeSequence)
+            cout << "P" << i << " ";
+        cout << endl;
+    } else {
+        cout << "UNSAFE\n";
+    }
+
+    int Q;
+    cin >> Q;
+
+    while (Q--) {
+        string op;
+        cin >> op;
+
+        if (op == "RQ") {   
+            int pid;
+            cin >> pid;
+
+            vector<int> request(m);
+            for (int j = 0; j < m; j++)
+                cin >> request[j];
+
+            bool valid = true;
+            for (int j = 0; j < m; j++)
+                if (request[j] > need[pid][j]) {
+                    cout << "ERROR: Request > Need\n";
+                    valid = false;
+                    break;
+                }
+
+            if (!valid) continue;
+            for (int j = 0; j < m; j++)
+                if (request[j] > available[j]) {
+                    cout << "DENIED: Insufficient Available\n";
+                    valid = false;
+                    break;
+                }
+
+            if (!valid) continue;
+
+            for (int j = 0; j < m; j++) {
+                available[j] -= request[j];
+                allocation[pid][j] += request[j];
+                need[pid][j] -= request[j];
+            }
+
+            if (isSafe(n, m, allocation, need, available, safeSequence, finished)) {
+                cout << "GRANTED\n";
+            } else {
+                cout << "DENIED (would make system UNSAFE)\n";
+                for (int j = 0; j < m; j++) {
+                    available[j] += request[j];
+                    allocation[pid][j] -= request[j];
+                    need[pid][j] += request[j];
+                }
+            }
+        }
+        else if (op == "FIN") {  
+            int pid;
+            cin >> pid;
+
+            bool canFinish = true;
+
+            for (int j = 0; j < m; j++) {
+                if (need[pid][j] != 0) {
+                    canFinish = false;
+                    break;
+                }
+            }
+
+            if (!canFinish) {
+                cout << "ERROR: Cannot FINISH (Need not zero)\n";
+                continue;
+            }
+            for (int j = 0; j < m; j++) {
+                available[j] += allocation[pid][j];
+                allocation[pid][j] = 0;
+            }
+
+            finished[pid] = true;
+
+            cout << "Process P" << pid << " FINISHED\n";
+        }
+        cout << "Available: ";
+        for (int x : available)
+            cout << x << " ";
+        cout << endl;
+        if (isSafe(n, m, allocation, need, available, safeSequence, finished)) {
+            cout << "SAFE\nSafe Sequence: ";
+            for (int i : safeSequence)
+                cout << "P" << i << " ";
+            cout << endl;
+        } else {
+            cout << "UNSAFE\n";
+        }
+    }
+
+    return 0;
+}
